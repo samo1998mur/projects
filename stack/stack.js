8367b36517b81e48c3d4319852318e302f6c8fd7
samo1998mur <samvelmuradyan572@gmail.com>
2021-12-16 20:29:23 +0400

    name

--- stack/stack.js
+++ stack/stack.js
@@ -1,22 +1,54 @@
+// function Stack() {
+//     this.data = [];
+//     this.index = 0;
+//     this.push = push;
+//     this.pop = pop;
+// }
+//
+// function push(element) {
+//     this.data[this.index++] = element;
+// }
+//
+// function pop() {
+//     return this.data[--this.index];
+// }
+// let s = new Stack()
+// s.push("Arsak")
+// s.push("Xoro")
+// s.push("Samo")
+// // s.push("Samoooooo")
+//
+// let betmen=  s.pop()
+// console.log(betmen)
+
 function Stack() {
-    this.data = [];
-    this.index = 0;
-    this.push = push;
-    this.pop = pop;
+    let data = [];
+    let index = 0;
+
+    return {
+        push: function(element) {
+            return  data[index++] = element;
+        },
+        pop: function() {
+           return  data[index--];
+        },
+        length: function() {
+            return index+1;
+        }
+    }
+
 }
 
-function push(element) {
-    this.data[this.index++] = element;
-}
+const klor = new Stack();
 
-function pop() {
-    return this.data[--this.index];
-}
-let s = new Stack()
-s.push("Arsak")
-s.push("Xoro")
-s.push("Samo")
-// s.push("Samoooooo")
+klor.push("a");
+klor.push("b");
+klor.push("c");
+klor.push("d");
+klor.push("k");
+klor.pop();
+klor.length();
+console.log(klor.pop())
+console.log(klor.length())
 
-let betmen=  s.pop()
-console.log(betmen)
+
